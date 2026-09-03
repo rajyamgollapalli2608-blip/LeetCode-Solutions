fcce9152ab6192e455f27d6fe1f1c935ddadd9e6
rajyamgollapalli2608-blip <rajyamgollapalli2608@gmail.com>
2026-09-03 21:14:57 +0530

    Time: 138 ms (11.58%) | Memory: 34.7 MB (97.56%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+        mn = float('inf')
+
+        for x in nums1:
+            if x % 2 == 1:
+                mn = min(mn, x)
+
+        for x in nums1:
+            if x % 2 == 0 and mn != float('inf') and x < mn:
+                return False
+
+        return True
+        
